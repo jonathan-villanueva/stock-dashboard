1786512c551e04b3895998e84bab346abad8fb79
Jonathan Villanueva <jtvillan@uci.edu>
2020-07-29 16:32:50 -0700

    Change stock font color based on current price

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -9,7 +9,8 @@ class Stock extends React.Component {
         this.state = {
             ticker: this.props.ticker,
             color: this.props.color,
-            price: 0
+            price: 0,
+            prev_price: 0
         }
 
         this.tick = this.tick.bind(this);
@@ -24,18 +25,37 @@ class Stock extends React.Component {
 
     tick() {
         var api_url = "https://cloud.iexapis.com/stable/stock/" + this.state.ticker + "/quote/latestPrice?token=pk_da183058d14d4805bf7614f9da7fe0ba"
+        var api_url_prev = "https://cloud.iexapis.com/stable/stock/" + this.state.ticker + "/previous?token=pk_da183058d14d4805bf7614f9da7fe0ba"
         fetch(api_url, {
             method: 'get'
           })
           .then(function(body){
-            return body.text(); // <--- THIS PART WAS MISSING
+            return body.text();
           }).then((data) => this.setState({price: data}));
+
+          fetch(api_url_prev, {
+            method: 'get'
+          })
+          .then(function(body){
+            return body.json();
+          }).then((data) => this.setState({prev_price: data.close}));
+
+        if(this.state.price > this.state.prev_price) {
+            this.setState({color: "green"})
+        } else
+        if(this.state.price === this.state.prev_price) {
+            this.setState({color: "black"})
+        } else
+        if(this.state.price < this.state.prev_price) {
+            this.setState({color: "red"})
+        }
+
     }
 
     render() {
         return (
             <div className="StockMain">
-                <li key={this.props.key} style={{backgroundColor: this.state.color}}>{this.state.ticker} <p className="price">{this.state.price}</p> </li>
+                <li className="StockContainer" key={this.props.key} style={{color: this.state.color}}>{this.state.ticker} <p className="price">{this.state.price}</p> </li>
             </div>
         )
     }
